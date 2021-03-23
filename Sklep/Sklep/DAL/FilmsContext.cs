f90f0bdc4cf6d3ba3ba7f32072cd1ede7d7c398e
Nagant1 <39465022+Nagant1@users.noreply.github.com>
2021-03-23 10:29:14 +0100

    Dodanie klas i FilmsContext

---
+++ Sklep/Sklep/DAL/FilmsContext.cs
@@ -0,0 +1,23 @@
+﻿using Sklep.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sklep.DAL
+{
+    public class FilmsContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public FilmsContext() : base("FilmsContext")
+        {
+
+        }
+
+
+    }
+}
