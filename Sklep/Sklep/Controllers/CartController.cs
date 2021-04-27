4bc08ccce1a86f214b01e80d557d040083f761b5
Nagant1 <39465022+Nagant1@users.noreply.github.com>
2021-04-27 11:23:22 +0200

    Mechanizm koszyka

---
+++ Sklep/Sklep/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using Sklep.DAL;
+using Sklep.Infrastructure;
+using Sklep.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sklep.Controllers
+{
+    public class CartController : Controller
+    {
+        CartManager cartManager;
+        FilmsContext db;
+        ISessionManager session;
+
+        public CartController()
+        {
+            db = new FilmsContext();
+            session = new SessionManager();
+            cartManager = new CartManager(db, session);
+        }
+        
+        // GET: Cart
+        public ActionResult Index()
+        {
+            CartViewModel cvm = new CartViewModel()
+            {
+                CartItems = cartManager.GetCartItems(),
+                TotalPrice = cartManager.GetCartValue()
+            };
+
+            return View();
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            cartManager.AddToCart(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
